adcf1f6c8a20287f968e6119d8b89c9d77014816
tiansanming <1072827745@qq.com>
2021-04-19 21:42:57 +0800

    自动化

---
+++ zidonghua.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from time import sleep
+driver = webdriver.Chrome()
+driver.get('https://blog.csdn.net/dpl12/article/details/102808985')
+shou = driver.find_element_by_xpath('//*[@id="csdn-toolbar"]/div/div/div[1]/ul/li[1]/a')
+shou.click()
+py = driver.find_element_by_xpath('//*[@id="floor-nav_62"]/div/div/div[2]/ul/li[1]/a')
+sleep(10)
+py.click()
+sleep(10)
+tui = driver.find_element_by_xpath('//*[@id="csdn-nav-second"]/div/div/ul/li[1]/a')
+sleep(10)
+tui.click()
+
+
