743ebf9e88dff40fd22ea17b4bf7225c75336799
tiansanming <1072827745@qq.com>
2021-06-06 15:15:47 +0800

    111

---
+++ test_demo.py
@@ -0,0 +1,29 @@
+# coding:utf-8
+
+
+def func(x):
+	return x + 3
+
+
+def test_func_0():
+	src = 0
+	expect = 3
+	assert func(src) == expect
+
+
+def test_func_1():
+	src = 0.1
+	expect = 3.1
+	assert func(src) == expect
+
+
+def test_func_2():
+	src = -1
+	expect = 0
+	assert func(src) == expect
+
+
+def test_func_3():
+	src = 5
+	expect = 7
+	assert func(src) == expect
